a0aeb447ed001a5348567213dfcfb8d144a18a23
Alexey Chernikov <iwizem@gmail.com>
2018-10-29 12:29:01 -0700

    Generate random integers with PRNG, instead of jsbn's default RNG

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const jsbn = require("jsbn")
+const jsbn = require("./jsbn")
 
 
-function generateRandomBigint(nBytes) {
+function generateRandomBigint(seedByteArray, nBytes) {
   let x = new Array(nBytes)
-  let rng = new jsbn.SecureRandom()
-  rng.nextBytes(x)
-
+  let prng = new jsbn.Arcfour()
+  prng.init(seedByteArray)
+  for (var i = x.length - 1; i >= 0; i--) {
+    x[i] = prng.next()
+  }
   return new jsbn.BigInteger(x)
 }
 
 
-randomInt = generateRandomBigint(128)
+randomInt = generateRandomBigint([23, 35, 63, 12], 128)
 console.log(randomInt.toString())
