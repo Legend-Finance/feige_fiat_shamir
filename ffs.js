bf40b2ce0418f2ac1d0a6333f5f43c16b0eff840
Alexey Chernikov <iwizem@gmail.com>
2018-10-31 12:37:35 -0700

    Split random seed array into 2 halves - for Si and pq

--- ffs.js
+++ ffs.js
@@ -3,10 +3,12 @@ const BigIntegerGenerator = require("./big_integer_generator")
 
 class Ffs {
   constructor(seedBytesArray, pqBytes, siBytes, k) {
+    if(length(seedBytesArray) < 2) throw "Must have at least 2 seed bytes";
     this.k = k;
-    let pqSeed = seedBytesArray;
-    let siSeed = seedBytesArray;
-    this.siGenerator = new BigIntegerGenerator(siSeed, siBytes)
+    let arrayMiddle = Math.floor(seedBytesArray.length/2);
+    let pqSeed = seedBytesArray.slice(0, arrayMiddle);
+    let siSeed = seedBytesArray.slice(arrayMiddle);
+    this.siGenerator = new BigIntegerGenerator(siSeed, siBytes);
     this.primes = new BigPrimeGenerator(new BigIntegerGenerator(pqSeed, pqBytes));
   }
 
@@ -19,7 +21,7 @@ class Ffs {
     if (this.n == undefined) throw "Choose N first";
     let S = [];
     while(S.length < this.k) {
-      S.push(this.siGenerator.nextCoprime(this.p, this.q))
+      S.push(this.siGenerator.nextCoprime(this.p, this.q));
     }
     return S;
   }
